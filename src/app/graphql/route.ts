09e1ca8448d2b4efb37b3d0d1f148160711dfe45
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2026-07-09 23:37:17 -0300

    liberando pagina do apollo para producao

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -1,10 +1,16 @@
 import { resolvers } from "@/graphql/resolvers";
 import { typeDefs } from "@/graphql/schema";
 import { ApolloServer, BaseContext } from "@apollo/server";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { NextRequest } from "next/server";
 
-const apollo = new ApolloServer<BaseContext>({ typeDefs, resolvers });
+const apollo = new ApolloServer<BaseContext>({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })],
+});
 
 const handler = startServerAndCreateNextHandler<NextRequest>(apollo, {
     context: async () => {
