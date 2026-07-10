9a9862548fc1b73dbc331bef9a4a3c0934f21063
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2026-07-09 22:47:56 -0300

    atualizando apollo server

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -15,4 +15,9 @@ const handler = startServerAndCreateNextHandler<NextRequest>(apollo, {
     },
 });
 
-export { handler as GET, handler as POST };
+export async function GET(request: NextRequest) {
+    return handler(request);
+}
+export async function POST(request: NextRequest) {
+    return handler(request);
+}
